ea3490349b7e94deb847fd52f6b48e186a120383
Albin Jordan <albin.jordan@better.care>
2021-03-02 17:11:35 +0100

    Combine Streams

--- apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { combineLatest, interval, Observable } from 'rxjs';
 import { Flight, FlightService } from '@flight-workspace/flight-lib';
-import { debounceTime, filter, map, pairwise, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, pairwise, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-workspace-flight-lookahead',
@@ -15,25 +15,48 @@ export class FlightLookaheadComponent implements OnInit {
   flights$: Observable<Flight[]>
   loading: boolean
   diff$: Observable<number>;
-
+  online: boolean = false;
+  online$: Observable<boolean>;
 
   constructor(protected flightService: FlightService) {
   }
 
   ngOnInit(): void {
     this.control = new FormControl();
-    this.flights$ = this.control.valueChanges.pipe(
+
+    this.online$
+      = interval(2000).pipe(
+      startWith(0),
+      map(_ => Math.random() < 0.5),
+      distinctUntilChanged(),
+      tap(value => this.online = value)
+    );
+
+    const input$ = this.control.valueChanges.pipe(
       filter(term => term?.length > 2),
-      debounceTime(500),
+      debounceTime(500));
+
+    this.flights$ = combineLatest([
+      input$,
+      this.online$
+    ]).pipe(
+      filter(([_, online]) => !!online),
+      map(([term, online]) => {
+        this.online = !!online;
+        return term;
+      }),
       tap(_ => this.loading = true),
       switchMap((term: string) => this.flightService.find(term)),
       tap(_ => this.loading = false)
-    )
+    );
+
 
     this.diff$ = this.flights$.pipe(
       pairwise(),
-      map( ([a, b]) => b.length - a.length )
+      map(([a, b]) => b.length - a.length)
     );
+
+
   }
 
 }

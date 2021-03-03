bc8f26b6c337ad9c36323fe84e38e8b6f0bf9928
Albin Jordan <albin.jordan@better.care>
2021-03-03 10:12:02 +0100

    Implementing Two-Way-Bindings for existing web components

--- apps/dashboard/src/app/custom-checkbox/custom-checkbox.directive.ts
+++ apps/dashboard/src/app/custom-checkbox/custom-checkbox.directive.ts
@@ -9,4 +9,11 @@ export class CustomCheckboxDirective {
   // TODO: Add @Output checkedChange to align with naming conventions
   // TODO: Add A HostListener for the custom element's changed event and trigger checkedChange
 
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  @HostListener('changed', ['$event'])
+  changed($event) {
+    this.checkedChange.next($event.detail);
+  }
+
 }
